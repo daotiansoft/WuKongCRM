28d959fa8c67a98b0f89b554bf16a924fa536f14
Your Name <you@example.com>
2026-03-21 09:09:48 +0800

    up

---
+++ ux/config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://127.0.0.1/index.php"'
+
+}
